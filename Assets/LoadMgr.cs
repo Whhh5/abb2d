d403a81967df233c446328ea903324e24ba8e18d
wangjiahao <2206222540@qq.com>
2024-10-17 22:35:58 +0800

    init

---
+++ Assets/LoadMgr.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadMgr
+{
+
+    private void LoadAsync<T>()
+        where T: class
+    {
+
+    }
+    private void Unload()
+    {
+
+    }
+}
